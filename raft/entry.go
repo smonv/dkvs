03becab27d6f94cd536ed803d99f7b6a29c97c7d
Tien Thanh <thanhvt1609@gmail.com>
2016-04-09 21:25:15 +0700

    rewrite

--- raft/entry.go
+++
@@ -1,8 +0,0 @@
-package raft
-
-// Entry store log entry
-type Entry struct {
-	Index   uint64
-	Term    uint64
-	Command []byte
-}
--- raft/transport.go
+++
@@ -1,7 +0,0 @@
-package raft
-
-// Transport provide interface for network transport
-type Transport interface {
-	AppendEntries(peer string, req *AppendEntryRequest) *AppendEntryResponse
-	RequestVote(peer string, req *RequestVoteRequest) *RequestVoteResponse
-}
